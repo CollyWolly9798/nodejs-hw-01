7651bbef6c14cc266978f854d2a4cc2fce056bc8
Oleksandr <collywolly9798@gmail.com>
2025-05-03 09:26:20 +0200

    hw

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+export const PATH_DB = path.join(dirname, '../db/db.json');
